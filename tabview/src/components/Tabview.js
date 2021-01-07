60043ae0b6945e5afaab9953a5ff2ab0ceeca4a4
mich <mich-cook@users.noreply.github.com>
2021-01-06 21:13:13 -0800

    wire the tab clicks to update the content area

--- tabview/src/components/Tabview.js
+++ tabview/src/components/Tabview.js
@@ -6,9 +6,15 @@ export default class Tabview extends Component {
 
   constructor(props) {
     super(props);
+    this.handleTabClick = this.handleTabClick.bind(this);
     this.state = {
       selected: this.props.initialtab
     };
+  }
+
+  handleTabClick(e) {
+    const intOffset = parseInt(e.target.parentElement.getAttribute("data-key"));
+    this.setState({ "selected": intOffset });
   }
 
   render() {
@@ -16,12 +22,12 @@ export default class Tabview extends Component {
       <div className="tabview">
         <ul className="tabs">
           {this.props.titles.map((title, i) => {
-            return <li key={i} className={((this.state.selected === i) ? `selected` : `` )}><button>{title}</button></li>;
+            return <li key={i} data-key={i} onClick={this.handleTabClick} className={((this.state.selected === i) ? `selected` : `` )}><button>{title}</button></li>;
           })}
         </ul>
         <ul className="contents">
           {this.props.contents.map((content, i) => {
-            return <li key={i} className={((this.state.selected === i) ? `selected` : `` )}>{content}</li>;
+            return <li key={i} data-key={i} className={((this.state.selected === i) ? `selected` : `` )}>{content}</li>;
           })}
         </ul>
       </div>
