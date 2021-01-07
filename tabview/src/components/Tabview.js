14b86648f669faee10b5bd2d4aaae621b30b5123
mich <mich-cook@users.noreply.github.com>
2021-01-06 21:10:48 -0800

    pipe initialtab prop to component state so it can be changed inside the component

--- tabview/src/components/Tabview.js
+++ tabview/src/components/Tabview.js
@@ -4,17 +4,24 @@ import "./Tabview.css";
 
 export default class Tabview extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: this.props.initialtab
+    };
+  }
+
   render() {
     return (
       <div className="tabview">
         <ul className="tabs">
           {this.props.titles.map((title, i) => {
-            return <li key={i} className={((this.props.initialtab === i) ? `selected` : `` )}><button>{title}</button></li>;
+            return <li key={i} className={((this.state.selected === i) ? `selected` : `` )}><button>{title}</button></li>;
           })}
         </ul>
         <ul className="contents">
           {this.props.contents.map((content, i) => {
-            return <li key={i} className={((this.props.initialtab === i) ? `selected` : `` )}>{content}</li>;
+            return <li key={i} className={((this.state.selected === i) ? `selected` : `` )}>{content}</li>;
           })}
         </ul>
       </div>
